23282703de6905e8bc204b9d42b650d8338f9138
Sanya P <sanya_ped@yahoo.com>
2022-02-20 01:02:24 +0700

    Finish

--- app.js
+++ app.js
@@ -1,24 +1,58 @@
-//#region 3rd Package
-
 const express = require('express');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
+const store = new MongoDBStore({
+	uri: 'mongodb://localhost:27017/express-session-demo',
+	collection: 'sessions'
+});
 
-//#endregion
+const User = require('./models/user');
 
 const app = express();
+
 app.set('view engine', 'ejs');
 
-//#region Midleware
-
 app.use(express.urlencoded({ extended: false }));
+app.use(
+	session({
+		secret: 'my secret',
+		resave: false,
+		saveUninitialized: false,
+		store: store
+	})
+);
 
-//#endregion
+app.get('/', (req, res) => res.render('index'));
+app.get('/login', (req, res) => res.render('login'));
+app.get('/register', (req, res) => res.render('register'));
 
-//#region Routes
+app.post('/login', async (req, res) => {
 
+    const user = await User.findOne({ username: req.body.username, password: req.body.password });
 
+    /*if (user) {
+        req.session.isLoggedIn = true;
+        req.session.user = { username: req.body.username, password: req.body.password };
+        req.session.save(err => {
+            if (err) console.log(err);
+            res.redirect('/');
+        });
+    }*/
 
-//#endregion
+    if (user) {
+        res.redirect('/');
+    }
+});
 
-app.listen(3000, () => {
+app.post('/register', async (req, res) => {
+
+    await User.create({ username: req.body.username, password: req.body.password });
+
+    res.redirect('/login');
+});
+
+app.listen(3001, () => {
+    mongoose.connect('mongodb://localhost:27017/express-session-demo');
     console.log(`Server Started @ ${new Date()}`);
 });
